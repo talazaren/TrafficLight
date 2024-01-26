116200a285b05ec092bdfe08de7ecdd7e1704fe2
Tatiana Lazarenko <tan.strebkova@gmail.com>
2024-01-26 17:08:35 +0300

    Made logic of traffic light

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -7,13 +7,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+    let statusOn: CGFloat = 1
+    let statusOff: CGFloat = 0.3
 
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    @IBOutlet var lightButton: UIButton!
+    
+    private var activeLight: TrafficLight?
+    
+    private enum TrafficLight {
+        case red
+        case yellow
+        case green
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLightView.alpha = statusOff
+        yellowLightView.alpha = statusOff
+        greenLightView.alpha = statusOff
     }
-
-
+    
+    @IBAction func lightButtonDidTapped() {
+        lightButton.setTitle("NEXT", for: .normal)
+        
+        switch activeLight {
+        case .red:
+            redLightView.alpha = statusOff
+            yellowLightView.alpha = statusOn
+            activeLight = .yellow
+        case .yellow:
+            yellowLightView.alpha = statusOff
+            greenLightView.alpha = statusOn
+            activeLight = .green
+        case .green:
+            greenLightView.alpha = statusOff
+            redLightView.alpha = statusOn
+            activeLight = .red
+        case nil:
+            redLightView.alpha = statusOn
+            activeLight = .red
+        }
+    }
 }
 
